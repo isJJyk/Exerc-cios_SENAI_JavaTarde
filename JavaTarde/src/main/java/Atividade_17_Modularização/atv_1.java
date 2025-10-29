2cb3a38c4c46b91c610b72f40a1f35a725fd124c
João Gabriel <joaogabriel6392@gmail.com>
2025-10-29 15:26:02 -0300

    Add files via upload

---
+++ JavaTarde/src/main/java/Atividade_17_Modularização/atv_1.java
@@ -0,0 +1,26 @@
+package atividade17;
+
+import java.util.Scanner;
+
+public class atv_1 {
+    public static void main(String[] args) {
+        String direction;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Para qual direção deseja seguir? (frente, direita, esquerda).");
+        direction = sc.nextLine();
+        String diracao=max(direction);
+        resultado(direction);}
+    public static String max( String direction) {
+        String resp;
+
+        if (direction.equalsIgnoreCase("Frente")) {
+            resp = "Movendo para frente.";
+        } else if (direction.equalsIgnoreCase("direita")) {
+            resp = "Movendo para a direita.";
+        } else {
+            resp = "Movendo para a esquerda.";
+        }return resp;
+    }public static void resultado(String resp){
+        System.out.println(resp);
+    }
+}
